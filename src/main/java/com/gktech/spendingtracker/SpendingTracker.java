c8a1f80d13732c46346c8a796090ba017f7e93de
flutter project practice <raksheetripathy6@gmail.com>
2024-10-28 18:24:31 +0530

    1st commit

---
+++ src/main/java/com/gktech/spendingtracker/SpendingTracker.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.gktech.spendingtracker;
+
+/**
+ *
+ * @author raksheetripathy
+ */
+public class SpendingTracker {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
